13e7f161b1daa437821fccaaad013245cd4c877b
mondher04000 <m_mameri@estin.dz>
2023-11-18 21:13:41 +0100

    adding charge balance withe cod and rewads

--- src/app/components/cod/Cod.jsx
+++ src/app/components/cod/Cod.jsx
@@ -1,15 +1,62 @@
+"use client";
+import { baseUrl } from "@/utils/constants";
+import axiosInstance from "@/utils/utils";
 import { useTranslations } from "next-intl";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Loader from "../loader/Loader";
 
 const Cod = ({ usecase }) => {
   const t = useTranslations("afterLogin");
+  const [cod, setCode] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  // handle submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const response = await axiosInstance.post(
+        `${baseUrl}/student/withdraw/charge-balance/cod`,
+        {
+          code: cod,
+        }
+      );
+      console.log(response);
+      if (response.status) {
+        toast.success("your request has been sent successfully");
+      }
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+      toast.error(error.response.data.message);
+    }
+  };
+
   return (
-    <form className="balance-input">
+    <form
+      className="balance-input"
+      onSubmit={usecase === "profile" ? handleSubmit : null}
+    >
+      <ToastContainer />
       <label htmlFor="balance">{t("your Cod code")}</label>
-      <input type="text" id="balance" />
+      <input
+        type="text"
+        id="balance"
+        onChange={(e) => setCode(e.target.value)}
+      />
       <label htmlFor="balance">{t("Promo code (optionel)")}</label>
       <input type="text" id="balance" />
-      <button className="pay hover" type="submit">
-        {usecase === "profile" ? t("edit") : t("subscribeBtn")}
+      <button className="pay hover" type="submit" disabled={isLoading}>
+        {isLoading ? (
+          <Loader />
+        ) : usecase === "profile" ? (
+          t("edit")
+        ) : (
+          t("subscribeBtn")
+        )}
       </button>
     </form>
   );
--- src/app/components/rewards/Rewards.jsx
+++ src/app/components/rewards/Rewards.jsx
@@ -1,13 +1,59 @@
+"use client";
 import { useTranslations } from "next-intl";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Loader from "../loader/Loader";
+import { baseUrl } from "@/utils/constants";
+import axiosInstance from "@/utils/utils";
 
 const Rewards = ({ usecase }) => {
   const t = useTranslations("afterLogin");
+  const [amount, setAmount] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  // handle submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    try {
+      const response = await axiosInstance .post(
+        `${baseUrl}/student/withdraw/charge-balance/reward`,
+        {
+          amount: amount,
+        }
+      );
+      console.log(response);
+      if (response.status) {
+        toast.success("your request has been sent successfully");
+      }
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+      toast.error(error.response.data.message);
+    }
+  };
   return (
-    <form className="balance-input">
+    <form
+      className="balance-input"
+      onSubmit={usecase === "profile" ? handleSubmit : null}
+    >
+      <ToastContainer />
       <label htmlFor="balance">{t("Promo code (optionel)")})</label>
-      <input type="text" id="balance" />
+      <input
+        type="text"
+        id="balance"
+        onChange={(e) => setAmount(e.target.value)}
+      />
       <button className="pay hover" type="submit">
-        {usecase === "profile" ? t("edit") : t("subscribeBtn")}
+        {isLoading ? (
+          <Loader />
+        ) : usecase === "profile" ? (
+          t("edit")
+        ) : (
+          t("subscribeBtn")
+        )}
       </button>
     </form>
   );
