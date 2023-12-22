54c760b4e3ec3c723b4009480b6c00f50c030306
mondher <110062316+mondher0@users.noreply.github.com>
2023-12-22 23:23:18 +0100

    adding side bar

--- src/app/components/nav-bar/NavBar.jsx
+++ src/app/components/nav-bar/NavBar.jsx
@@ -5,40 +5,55 @@ import "./NavBar.css";
 import { useTranslations } from "next-intl";
 import { useLocale } from "next-intl";
 import { useRouter } from "next/navigation";
+import { CiMenuBurger } from "react-icons/ci";
+import SideBar from "./SideBar";
+import { useState } from "react";
 
 const NavBar = () => {
   const locale = useLocale();
   const t = useTranslations("Index");
   const router = useRouter();
+  const [isSidebarOpen, setIsSideBarOpen] = useState();
   return (
-    <nav className={locale === "ar" ? "nav-bar-reversed" : ""}>
-      <Logo Click="nav-logo" />
-      <ul className={locale === "ar" ? "links reversed" : "links"}>
-        <Links link="/" text={t("home")} />
-        <Links link="/#about" text={t("about")} />
-        <Links link="/#welcome" text={t("welcome")} />
-        <Links link="/#features" text={t("features")} />
-      </ul>
-      <div className="nav-btns">
-        <button
-          className="outline log"
-          onClick={() => {
-            router.push("/login");
-          }}
-        >
-          {t("login")}
-        </button>
-        <button
-          className="inline log"
-          onClick={() => {
-            router.push("/register");
-          }}
-        >
-          {t("signUp")}
-        </button>
-        <button className="inline dow">{t("download")}</button>
-      </div>
-    </nav>
+    <>
+      <nav className={locale === "ar" ? "nav-bar-reversed" : ""}>
+        <Logo Click="nav-logo" />
+        <ul className={locale === "ar" ? "links reversed" : "links"}>
+          <Links link="/" text={t("home")} />
+          <Links link="/#about" text={t("about")} />
+          <Links link="/#welcome" text={t("welcome")} />
+          <Links link="/#features" text={t("features")} />
+        </ul>
+        <div className="nav-btns">
+          <button
+            className="outline log"
+            onClick={() => {
+              router.push("/login");
+            }}
+          >
+            {t("login")}
+          </button>
+          <button
+            className="inline log"
+            onClick={() => {
+              router.push("/register");
+            }}
+          >
+            {t("signUp")}
+          </button>
+          <CiMenuBurger
+            className="burger"
+            onClick={() => {
+              setIsSideBarOpen("show-sidebar");
+            }}
+          />
+        </div>
+      </nav>
+      <SideBar
+        isSidebarOpen={isSidebarOpen}
+        setIsSideBarOpen={setIsSideBarOpen}
+      />
+    </>
   );
 };
 
---
+++ src/app/components/nav-bar/SideBar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Links from "../link/Links";
+import { FaTimes } from "react-icons/fa";
+import Logo from "@/atoms/logo/Logo";
+import "./NavBar.css";
+import { useRouter } from "next/navigation";
+import { useLocale, useTranslations } from "next-intl";
+
+const SideBar = ({ isSidebarOpen, setIsSideBarOpen }) => {
+  const locale = useLocale();
+  const t = useTranslations("Index");
+  const router = useRouter();
+  return (
+    <aside className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+      <div className="sidebar-header">
+        <Logo Click="nav-logo" />
+        <button className="close-btn" onClick={() => setIsSideBarOpen(false)}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className={locale === "ar" ? "aside-links reversed" : "aside-links"}>
+        <Links link="/" text={t("home")} />
+        <Links link="/#about" text={t("about")} />
+        <Links link="/#welcome" text={t("welcome")} />
+        <Links link="/#features" text={t("features")} />
+      </ul>
+      <div className="dow-container">
+        <button className="aside-dow">{t("download")}</button>
+      </div>
+    </aside>
+  );
+};
+
+export default SideBar;
